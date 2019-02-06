15e4181246e83f9485d154555679f2d6c9920c8a
h22fred <4137037+h22fred@users.noreply.github.com>
2019-02-06 11:18:16 +0100

    Add files via upload

---
+++ webcams.php
@@ -0,0 +1,50 @@
+{
+	"webcamsAll" : [
+		{       
+			"Name": "Aeroport",
+			"WebcamDir": 4,
+			"WebcamDirI": 3,
+			"Order": 10
+		},
+		{          
+			"Name": "Meyrin",		
+			"WebcamDir": 5,
+			"WebcamDirI": 6,
+			"Order": 20
+		}
+		, {
+			"Name": "Vernier", 
+			"WebcamDir": 8,
+			"WebcamDirI": 7,
+			"Order": 30
+		}
+		, {
+			"Name": "Bernex",
+			"WebcamDir": 10,
+			"WebcamDirI": 9,
+			"Order": 40
+		}
+		, {
+			"Name": "Confignon",
+			"WebcamDir": 19,
+			"WebcamDirI": 20,
+			"Order": 50
+		}
+		, {
+			"Name": "Perly",
+			"WebcamDir": 12,
+			"WebcamDirI": 11,
+			"Order": 60
+		}, {
+			"Name": "Bardonnex",
+			"WebcamDir": 14,
+			"WebcamDirI": 13,
+			"Order": 70
+		}, {
+			"Name": "Bardonnex inverse",
+			"WebcamDir": 13,
+			"WebcamDirI": 14,
+			"Order": 80
+		}
+	]
+}
---
+++ webcamsC.php
@@ -0,0 +1,39 @@
+{	
+	"webcamsCentreAll" : [
+        {
+            "Name": "Route des Jeunes",
+			"WebcamDir": 18,
+			"WebcamDirI": 17,
+            "Order": 10
+        }
+       , {
+            "Name": "Bachet de Pesay",
+			"WebcamDir": 16,
+			"WebcamDirI": 15,
+            "Order": 20
+        }
+        , {
+            "Name": "Tunnel Saconnex d’Arve",
+			"WebcamDir": 23,
+			"WebcamDirI": 22,
+            "Order": 30
+        }
+        , {
+            "Name": "Saconnex d’Arve",
+			"WebcamDir": 21,
+			"WebcamDirI": 24,
+            "Order": 40
+        }
+		,{
+            "Name": "Bardonnex",
+			"WebcamDir": 14,
+			"WebcamDirI": 13,
+            "Order": 50
+        }, {
+            "Name": "Bardonnex inverse",
+			"WebcamDir": 13,
+			"WebcamDirI": 14,
+            "Order": 60
+        }
+    ]
+}
